6bd1be5bb07cf1c7b2e88b3aa52de61029c36e4b
starsubsp <naledimgedezi@gmail.com>
2026-04-02 21:53:23 +0200

    feat: implement navigation and routing for products and categories

--- src/ProductCatalogManager.Client/ProductCatalogManager.UI/src/app/app.routes.ts
+++ src/ProductCatalogManager.Client/ProductCatalogManager.UI/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductsPage } from './pages/products-page/products-page';
+import { ProductFormComponent } from './components/product-form/product-form';
+import { CategoriesPage } from './pages/categories-page/categories-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsPage },
+  { path: 'products/new', component: ProductFormComponent },
+  { path: 'products/edit/:id', component: ProductFormComponent },
+  { path: 'categories', component: CategoriesPage },
+];
